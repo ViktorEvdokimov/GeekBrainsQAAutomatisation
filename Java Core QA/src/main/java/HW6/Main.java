0de32b872d18bd8450358b2c63abf31b2a01b979
ViktorEvdokimov <74970962+ViktorEvdokimov@users.noreply.github.com>
2021-12-09 22:29:46 +0500

    HW6

---
+++ Java Core QA/src/main/java/HW6/Main.java
@@ -0,0 +1,54 @@
+package HW6;
+
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+
+public class Main {
+
+    private static final String BASE_HOST = "dataservice.accuweather.com";
+    private static final String FORECAST = "forecasts";
+    private static final String API_VERSION = "v1";
+    private static final String FORECAST_TYPE = "daily";
+    private static final String FORECAST_PERIOD = "5day";
+
+    private static final String SAINT_PETERSBURG_KEY = "295212";
+    private static final String API_KEY = "zdZjGDkMvstxPVCqP0ZOcWk7I3Gxgcpy";
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        // Сегментированное построение URL
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(BASE_HOST)
+                .addPathSegment(FORECAST)
+                .addPathSegment(API_VERSION)
+                .addPathSegment(FORECAST_TYPE)
+                .addPathSegment(FORECAST_PERIOD)
+                .addPathSegment(SAINT_PETERSBURG_KEY)
+                .addQueryParameter("apikey", API_KEY)
+                .addQueryParameter("language", "ru-ru")
+                .addQueryParameter("metric", "true")
+                .build();
+
+        System.out.println(url.toString());
+
+        // При необходимости указать заголовки
+        Request requesthttp = new Request.Builder()
+                .addHeader("accept", "application/json")
+                .url(url)
+                .build();
+
+        String jsonResponse = client.newCall(requesthttp).execute().body().string();
+        System.out.println(jsonResponse);
+    }
+
+
+}
