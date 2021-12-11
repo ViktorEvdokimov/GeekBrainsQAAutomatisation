67d535a2efd885d6526686e0e47d82ccb214afd8
ViktorEvdokimov <74970962+ViktorEvdokimov@users.noreply.github.com>
2021-12-11 19:48:29 +0500

    HW7

--- Java Core QA/src/main/java/HW7/AccuWeatherProvider.java
+++ Java Core QA/src/main/java/HW7/AccuWeatherProvider.java
@@ -1,6 +1,8 @@
 package HW7;
 
 import HW7.enums.Periods;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -8,14 +10,17 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AccuWeatherProvider implements WeatherProvider {
 
     private static final String BASE_HOST = "dataservice.accuweather.com";
     private static final String FORECAST_ENDPOINT = "forecasts";
     private static final String CURRENT_CONDITIONS_ENDPOINT = "currentconditions";
+    private static final String DAILY_CONDITIONS_ENDPOINT = "daily";
     private static final String API_VERSION = "v1";
     private static final String API_KEY = ApplicationGlobalState.getInstance().getApiKey();
+
 
     private final OkHttpClient client = new OkHttpClient();
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -39,10 +44,33 @@ public class AccuWeatherProvider implements WeatherProvider {
                 .build();
 
             Response response = client.newCall(request).execute();
-            System.out.println(response.body().string());
+//            System.out.println(response.body().string());
+            printFormattedWeatherByDay(response.body().string());
             // TODO: Сделать в рамках д/з вывод более приятным для пользователя.
             //  Создать класс WeatherResponse, десериализовать ответ сервера в экземпляр класса
             //  Вывести пользователю только текущую температуру в C и сообщение (weather text)
+        } else if(periods.equals(Periods.FIVE_DAYS)) {
+            HttpUrl url = new HttpUrl.Builder()
+                    .scheme("http")
+                    .host(BASE_HOST)
+                    .addPathSegment(FORECAST_ENDPOINT)
+                    .addPathSegment(API_VERSION)
+                    .addPathSegment(DAILY_CONDITIONS_ENDPOINT)
+                    .addPathSegment("5day")
+                    .addPathSegment(cityKey)
+                    .addQueryParameter("apikey", API_KEY)
+                    .addQueryParameter("language", "ru-ru")
+                    .addQueryParameter("metric", "true")
+                    .build();
+
+            Request request = new Request.Builder()
+                    .addHeader("accept", "application/json")
+                    .url(url)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+//            System.out.println(response.body().string());
+            printFormattedWeatherByFiveDays(response.body().string());
         }
     }
 
@@ -81,5 +109,48 @@ public class AccuWeatherProvider implements WeatherProvider {
         } else throw new IOException("Server returns 0 cities");
 
         return objectMapper.readTree(jsonResponse).get(0).at("/Key").asText();
+    }
+
+    private void printFormattedWeatherByDay(String message){
+        try {
+            JsonNode parentNode = objectMapper.readTree(message);
+            for (Iterator<JsonNode> it = parentNode.elements(); it.hasNext(); ) {
+                JsonNode element = it.next();
+                if (element.at("/IsDayTime").asBoolean()){
+                    System.out.print("Weather in day of ");
+                } else System.out.print("Weather in night of ");
+                System.out.println(element.at("/LocalObservationDateTime").asText().substring(0,10));
+                System.out.print(element.at("/WeatherText").asText());
+                if (element.at("/HasPrecipitation").asBoolean()){
+                    System.out.print(", possible precipitation in the form ");
+                    System.out.println(element.at("/PrecipitationType").asText());
+                } else System.out.println(" without precipitation");
+                System.out.print("Temperature: ");
+                System.out.println(element.at("/Temperature/Metric/Value").asText() + "°C");
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("SWW when parse message", e);
+        }
+    }
+
+    private void printFormattedWeatherByFiveDays (String message){
+        try {
+            JsonNode parentNode = objectMapper.readTree(message).at("/DailyForecasts");
+
+            for (Iterator<JsonNode> it = parentNode.elements(); it.hasNext(); ) {
+                JsonNode element = it.next();
+                System.out.print("Weather in day of ");
+                System.out.println(element.at("/Date").asText().substring(0,10));
+                System.out.println(element.at("/Day/IconPhrase").asText());
+                System.out.print("Weather in night of ");
+                System.out.println(element.at("/Date").asText().substring(0,10));
+                System.out.println(element.at("/Night/IconPhrase").asText());
+                System.out.print("Temperature: ");
+                System.out.println(element.at("/Temperature/Minimum/Value").asText() + " - " + element.at("/Temperature/Maximum/Value").asText() + "°C");
+                System.out.println();
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("SWW when parse message", e);
+        }
     }
 }
--- Java Core QA/src/main/java/HW7/ApplicationGlobalState.java
+++ Java Core QA/src/main/java/HW7/ApplicationGlobalState.java
@@ -4,7 +4,7 @@ public final class ApplicationGlobalState {
 
     private static ApplicationGlobalState INSTANCE;
     private String selectedCity = null;
-    private final String API_KEY = "0d1tNZJPfzzT3qGokM18FGGxAUpt7hpj";
+    private final String API_KEY = "zdZjGDkMvstxPVCqP0ZOcWk7I3Gxgcpy";
 
     private ApplicationGlobalState() {
     }
